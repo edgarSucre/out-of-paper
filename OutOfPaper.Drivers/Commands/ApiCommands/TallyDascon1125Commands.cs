bf61e3c46e8e509c8d047c24431eeaa3c83ad9f8
Edgar Sucre <edgar.sucre@controlrisks.com>
2018-03-12 21:17:44 -0500

    started working on commands

---
+++ OutOfPaper.Drivers/Commands/ApiCommands/TallyDascon1125Commands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfPaper.Drivers.Commands.ApiCommands
+{
+    class TallyDascon1125Commands : ICommandable
+    {
+        #region commands string values
+        string LOGIN_CACHIER = "5";
+        string LOGOUT_CACHIER = "6";
+        #endregion
+
+        TallyDascon1125Commands()
+        {
+            //Injectar instancia de impresora
+        }
+
+        public void LoginCachier(int cachierPassword)
+        {
+            var command = LOGIN_CACHIER + cachierPassword;
+            throw new NotImplementedException();
+        }
+
+        public void LogoutCachier()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OutOfPaper.Drivers/Commands/ICommandable.cs
@@ -0,0 +1,9 @@
+﻿namespace OutOfPaper.Drivers.Commands
+{
+    interface ICommandable
+    {
+        void LoginCachier(int cachierPassword);
+        void LogoutCachier();
+
+    }
+}
